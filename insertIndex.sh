8179a5ff6a1bf5d70c1c976725bf0ffd6651d14a
wubinTRON <44354524+wubinTRON@users.noreply.github.com>
2019-01-17 17:13:01 +0800

    Create insertIndex.sh

---
+++ insertIndex.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+mongoIp=`cat config.conf | grep mongo.host | awk -F '=' '{print $2}'`
+mongoPort=`cat config.conf | grep mongo.port | awk -F '=' '{print $2}'`
+dbname=`cat config.conf | grep mongo.dbname | awk -F '=' '{print $2}'`
+userName=`cat config.conf | grep mongo.username | awk -F '=' '{print $2}'`
+password=`cat config.conf | grep mongo.password | awk -F '=' '{print $2}'`
+
+#mongo ip+port
+mongodb='mongo '$mongoIp':'$mongoPort
+$mongodb <<EOF
+use ${dbname}
+db.auth("${userName}", "${password}")
+db.contractevent.ensureIndex({contractAddress:-1})
+db.transaction.ensureIndex({fromAddress:-1})
+db.transaction.ensureIndex({contractType:-1})
+db.transaction.ensureIndex({toAddress:-1})
+db.transaction.ensureIndex({timeStamp:-1})
+db.transaction.ensureIndex({transactionId:-1})
+db.transaction.ensureIndex({blockHash:-1})
+db.transaction.ensureIndex({blockNumber:-1})
+db.transaction.ensureIndex({contractAddress:-1})
+db.transaction.ensureIndex({assetName:-1})
+db.contractevent.ensureIndex({eventSignature:-1})
+db.contractevent.ensureIndex({transactionId:-1})
+db.contractevent.ensureIndex({contractAddress:-1})
+db.contractevent.ensureIndex({blockNumber:-1})
+db.contractevent.ensureIndex({timeStamp:-1})
+db.contractevent.ensureIndex({eventName:-1})
+EOF
