b562725f6838ef75bc44826d54ed4ba7421fc6b8
wubinTRON <44354524+wubinTRON@users.noreply.github.com>
2019-01-17 23:29:07 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [[ "$?" -ne 0 ]] ; then
   echo 'could not perform tests'; exit $rc
 else
     while true; do
-      pid=`ps -ef |grep troneventquery |grep -v grep |awk '{print $2}'`
+      pid=`ps -ef | grep troneventquery | grep -v grep | awk '{print $2}'`
       if [ -n "$pid" ]; then
         kill -15 $pid
         echo "ending tron event query process"
@@ -15,7 +15,7 @@ else
         break
       fi
     done
-     nohup java -jar target/troneventquery-1.0.0-SNAPSHOT.jar &
+    nohup java -jar target/troneventquery-1.0.0-SNAPSHOT.jar 2>&1 &
     sleep 10
     echo "ok!"
 fi
