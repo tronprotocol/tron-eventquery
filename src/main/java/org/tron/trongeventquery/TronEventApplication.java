a3915f841a8c1db73d35d43752aa0026d58d09d6
wubinTron <wubin1@tron.network>
2020-02-27 18:42:31 +0800

    remove corsFilter

--- src/main/java/org/tron/trongeventquery/TronEventApplication.java
+++ src/main/java/org/tron/trongeventquery/TronEventApplication.java
@@ -37,7 +37,21 @@ public class TronEventApplication {
     SpringApplication.run(TronEventApplication.class, args);
     shutdown();
   }
-  
+
+  @Bean
+  public FilterRegistrationBean corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+    bean.setOrder(0);
+    return bean;
+  }
+
   @Bean
   public MongoTemplate mongoTemplate(
       @Value("${mongo.host}")String mongodbIp, @Value("${mongo.dbname}")String mongodbDbName,
